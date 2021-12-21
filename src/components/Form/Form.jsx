bb34c8ac6487cf5e924e18230f4ae1c4de97959e
MaksimEdam <Edamenko6@gmail.com>
2021-12-21 11:33:53 +0200

    hook form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,6 +1,72 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import s from './Form.module.css';
-class Form extends Component {
+
+function Form({ onSubmit }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const handleChange = e => {
+    const { name, value } = e.currentTarget;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+    onSubmit(name, number);
+    resetState();
+  };
+
+  const resetState = () => {
+    setName('');
+    setNumber('');
+  };
+  return (
+    <form onSubmit={handleSubmit} className="test3">
+      <div className={s.inputWrapper}>
+        <label className={s.Label}>
+          Name
+          <input
+            type="text"
+            value={name}
+            onChange={handleChange}
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+      </div>
+      <div className={s.inputWrapper}>
+        <label className={s.Label}>
+          Number
+          <input
+            type="tel"
+            value={number}
+            onChange={handleChange}
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
+      </div>
+      <button className={s.FormButton} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
+}
+
+export default Form;
+
+/* class Form extends Component {
   state = {
     name: '',
     number: '',
@@ -62,4 +128,4 @@ class Form extends Component {
   }
 }
 
-export default Form;
+export default Form; */
